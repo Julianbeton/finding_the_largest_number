f29b31782767c6b0d495c680bce2f1b38f15c9c9
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-07 23:59:26 +0800

    pseudocode

--- finding_the_largest_number.py
+++ finding_the_largest_number.py
@@ -1,1 +1,10 @@
-#Ask user to input 3 numbers. Find and print the biggest number using only if statement.
+#Assignment 4
+#Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
+#pseudocode
+#import tkinter
+#ask user to input number 1
+#ask user to input number 2
+#ask user to input number 3
+#find the largest number among the 3 numbers
+#Display the largest number
