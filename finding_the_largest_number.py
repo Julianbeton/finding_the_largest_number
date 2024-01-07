9b469eed53fcb3cc73f7b1d796b6112f72a42d8d
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-08 00:54:15 +0800

    first commit

--- finding_the_largest_number.py
+++ finding_the_largest_number.py
@@ -3,8 +3,26 @@
 
 #pseudocode
 #import tkinter
+import tkinter as tk
+from tkinter import messagebox
+
+#function
+def finding_the_largest_number():
 #ask user to input number 1
+    first_number = float(input("Input the first number: "))
 #ask user to input number 2
+    second_number = float(input("Input the second number: "))
 #ask user to input number 3
+    third_number = float(input("Input the third number: "))
 #find the largest number among the 3 numbers
+    if first_number >= second_number and first_number >= third_number:
+        largest_number = first_number
+    elif second_number >= first_number and second_number >= third_number:
+        largest_number = second_number
+    else: 
+        largest_number = third_number
+
 #Display the largest number
+    print("The Largest Number is:", largest_number)
+
+finding_the_largest_number()
